738ed418fcf84f1b0a939da2f4e1f0083925a475
Wonda <jk4837@yahoo.com.tw>
2018-11-14 18:57:15 +0800

    Rename ShowDefinition to ShowDefinitionEx Using built-in logic to pre-filter hover symbol Add testing structure Fix parsing logic Adjuct popup view

---
+++ tests/test.py
@@ -0,0 +1,23 @@
+def funcN1(self):
+		aabbcc
+
+class ClassA(sublime_plugin.WindowCommand):
+	def funcA1(self):
+		aabbcc
+	def funcA2(self):
+		aabbcc
+
+def funcN2(self):
+		aabbcc
+
+class ClassB(sublime_plugin.WindowCommand):
+	class ClassC(sublime_plugin.WindowCommand):
+		def funcC1(self):
+			aabbcc
+		def funcC2(self):
+			aabbcc
+	def funcB1(self):
+		aabbcc
+
+def funcN3(self):
+		aabbcc
